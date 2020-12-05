f34482d17bbc5c74f095d0a828acdb52ca6f0505
David Daly <dalyd14@gmail.com>
2020-12-05 15:19:35 -0500

    set up the first page for the timeline

--- assets/js/api-calls.js
+++ assets/js/api-calls.js
@@ -1,49 +1,55 @@
 var getYearTopMovies = function(year) {
-    var sectionsUrl = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + year + "_in_film&prop=sections&origin=*&formatversion=2"
-    fetch(sectionsUrl).then(function(response){
-        if(response.ok) {
-            response.json().then(function(sections){
-                if("error" in sections) {
-                    console.log({
-                        Response: "False",
-                        Error: "Incorrect year input!"
-                    })
-                } else {
-                    sections = sections.parse.sections
-                    var secNum = null
-                    for(var i = 0; i < sections.length; i++) {
-                        if(
-                            sections[i].line === "Highest-grossing films" || 
-                            sections[i].line === "Highest-grossing films (U.S.)" ||
-                            sections[i].line === "Top-grossing films (U.S.)" ||
-                            sections[i].line === "Top-grossing films"){
-                            secNum = sections[i].index
-                            break;
+    return new Promise(resolve => {
+        var sectionsUrl = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + year + "_in_film&prop=sections&origin=*&formatversion=2"
+        fetch(sectionsUrl).then(function(response){
+            if(response.ok) {
+                response.json().then(function(sections){
+                    if("error" in sections) {
+                        resolve({
+                            Response: "False",
+                            Error: "Incorrect year input!"
+                        })
+                    } else {
+                        sections = sections.parse.sections
+                        var secNum = null
+                        for(var i = 0; i < sections.length; i++) {
+                            if(
+                                sections[i].line === "Highest-grossing films" || 
+                                sections[i].line === "Highest-grossing films (U.S.)" ||
+                                sections[i].line === "Top-grossing films (U.S.)" ||
+                                sections[i].line === "Top-grossing films"){
+                                secNum = sections[i].index
+                                break;
+                            }
                         }
+                        fetch("https://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + year + "_in_film&prop=text&section=" + secNum + "&origin=*&formatversion=2").then(function(response){
+                            if(response.ok){
+                                response.json().then(function(data){
+                                    data = jQuery.parseHTML(data.parse.text)
+                                    data = $(data[0]).find("table")[0]
+                                    data = $(data).children()[1]
+                                    data = $(data).children()
+                                    var movies = []
+                                    for (var i = 1; i < data.length; i++) {
+                                        var movieHtml = $(data[i]).children()
+                                        var movieTitle = $(movieHtml[1]).text().trim()
+                                        movies.push({
+                                            movieTitle: movieTitle,
+                                            movieYear: year
+                                        })
+                                    }
+                                    async function asyncCalltoOMDB() {
+                                        var res = await omdbApiCalls(movies)
+                                        resolve(res)
+                                    }
+                                    asyncCalltoOMDB()
+                                })
+                            }
+                        })                    
                     }
-                    fetch("https://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + year + "_in_film&prop=text&section=" + secNum + "&origin=*&formatversion=2").then(function(response){
-                        if(response.ok){
-                            response.json().then(function(data){
-                                data = jQuery.parseHTML(data.parse.text)
-                                data = $(data[0]).find("table")[0]
-                                data = $(data).children()[1]
-                                data = $(data).children()
-                                var movies = []
-                                for (var i = 1; i < data.length; i++) {
-                                    var movieHtml = $(data[i]).children()
-                                    var movieTitle = $(movieHtml[1]).text().trim()
-                                    movies.push({
-                                        movieTitle: movieTitle,
-                                        movieYear: year
-                                    })
-                                }
-                                omdbApiCalls(movies)
-                            })
-                        }
-                    })                    
-                }
-            })
-        }
+                })
+            }
+        })
     })
 }
 
@@ -70,25 +76,25 @@ var getDecadeTopMovies = function(decade) {
                         fetch("https://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + decade + "s_in_film&prop=text&section=" + secNum + "&origin=*&formatversion=2").then(function(response){
                             if(response.ok){
                                 response.json().then(function(data) {
-                                data = jQuery.parseHTML(data.parse.text)
-                                data = $(data[0]).find("table")[0]
-                                data = $(data).children()[1]
-                                data = $(data).children()
-                                var movies = []
-                                for (var i = 1; i < data.length; i++) {
-                                    var movieHtml = $(data[i]).children()
-                                    var movieTitle = $(movieHtml[1]).text().trim()
-                                    var movieYear = $(movieHtml[movieHtml.length - 2]).text().trim()
-                                    movies.push({
-                                        movieTitle: movieTitle,
-                                        movieYear: movieYear
-                                    })
-                                }
-                                async function asyncCalltoOMDB() {
-                                    var res = await omdbApiCalls(movies)
-                                    resolve(res)
-                                }
-                                asyncCalltoOMDB()
+                                    data = jQuery.parseHTML(data.parse.text)
+                                    data = $(data[0]).find("table")[0]
+                                    data = $(data).children()[1]
+                                    data = $(data).children()
+                                    var movies = []
+                                    for (var i = 1; i < data.length; i++) {
+                                        var movieHtml = $(data[i]).children()
+                                        var movieTitle = $(movieHtml[1]).text().trim()
+                                        var movieYear = $(movieHtml[movieHtml.length - 2]).text().trim()
+                                        movies.push({
+                                            movieTitle: movieTitle,
+                                            movieYear: movieYear
+                                        })
+                                    }
+                                    async function asyncCalltoOMDB() {
+                                        var res = await omdbApiCalls(movies)
+                                        resolve(res)
+                                    }
+                                    asyncCalltoOMDB()
                                 })
                             }
                         })
@@ -115,7 +121,6 @@ var singleOmdbApiCall = function(title) {
             Error: "Search term was not a string!"
         })
     }
-
 }
 
 var omdbApiCalls = function(movies) {
