015da99150f5d439b9fa7ff4f4ffeb20566408d3
David Daly <dalyd14@gmail.com>
2020-12-05 12:35:04 -0500

    resolved merging issues

--- assets/js/api-calls.js
+++ assets/js/api-calls.js
@@ -53,52 +53,52 @@ var getDecadeTopMovies = function(decade) {
         fetch(sectionsUrl).then(function(response){
             if(response.ok) {
                 response.json().then(function(sections){
-                  if("error" in sections) {
-                      console.log({
-                          Response: "False",
-                          Error: "Incorrect decade input!"
-                      })
-                  } else {
-                      sections = sections.parse.sections
-                      var secNum = null
-                      for(var i = 0; i < sections.length; i++) {
-                          if(sections[i].line === "Highest-grossing films"){
-                              secNum = sections[i].index
-                              break;
-                          }
-                      }
-                      fetch("https://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + decade + "s_in_film&prop=text&section=" + secNum + "&origin=*&formatversion=2").then(function(response){
-                          if(response.ok){
-                              response.json().then(function(data){
-                                  data = jQuery.parseHTML(data.parse.text)
-                                  data = $(data[0]).find("table")[0]
-                                  data = $(data).children()[1]
-                                  data = $(data).children()
-                                  var movies = []
-                                  for (var i = 1; i < data.length; i++) {
-                                      var movieHtml = $(data[i]).children()
-                                      var movieTitle = $(movieHtml[1]).text().trim()
-                                      var movieYear = $(movieHtml[movieHtml.length - 2]).text().trim()
-                                      movies.push({
-                                          movieTitle: movieTitle,
-                                          movieYear: movieYear
-                                      })
-                                  }
-                                  async function asyncCalltoOMDB() {
-                                      var res = await omdbApiCalls(movies)
-                                      resolve(res)
-                                  }
-                                  asyncCalltoOMDB()
-                              })
-                          }
-                       }
-                    })
+                    if("error" in sections) {
+                        console.log({
+                            Response: "False",
+                            Error: "Incorrect decade input!"
+                        })
+                    } else {
+                        sections = sections.parse.sections
+                        var secNum = null
+                        for(var i = 0; i < sections.length; i++) {
+                            if(sections[i].line === "Highest-grossing films"){
+                                secNum = sections[i].index
+                                break;
+                            }
+                        }
+                        fetch("https://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + decade + "s_in_film&prop=text&section=" + secNum + "&origin=*&formatversion=2").then(function(response){
+                            if(response.ok){
+                                response.json().then(function(data) {
+                                data = jQuery.parseHTML(data.parse.text)
+                                data = $(data[0]).find("table")[0]
+                                data = $(data).children()[1]
+                                data = $(data).children()
+                                var movies = []
+                                for (var i = 1; i < data.length; i++) {
+                                    var movieHtml = $(data[i]).children()
+                                    var movieTitle = $(movieHtml[1]).text().trim()
+                                    var movieYear = $(movieHtml[movieHtml.length - 2]).text().trim()
+                                    movies.push({
+                                        movieTitle: movieTitle,
+                                        movieYear: movieYear
+                                    })
+                                }
+                                async function asyncCalltoOMDB() {
+                                    var res = await omdbApiCalls(movies)
+                                    resolve(res)
+                                }
+                                asyncCalltoOMDB()
+                                })
+                            }
+                        })
+                    }
                 })
             }
-        })        
+        })
     })
 }
-
+    
 var singleOmdbApiCall = function(title) {
     if(typeof title === "string") {
         fetch("https://www.omdbapi.com/?t=" + title.trim().split(' ').join('+') + "&apikey=f92c60e5").then(function(response){
