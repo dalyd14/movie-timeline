3654b9640294cc6066a336d62f4498b3849b0bd5
David Daly <dalyd14@gmail.com>
2020-12-10 13:15:16 -0500

    added comments to the api call script

--- assets/js/api-calls.js
+++ assets/js/api-calls.js
@@ -1,5 +1,8 @@
+// this function calls the wiki api and returns an array of movies that were found in the highest grossing films page
 var getYearTopMovies = function(year) {
+    // create new promise that will only resolve when the results are received
     return new Promise(resolve => {
+        // the link we are calling from the api to see what section number we need to look at
         var sectionsUrl = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + year + "_in_film&prop=sections&origin=*&formatversion=2"
         fetch(sectionsUrl).then(function(response){
             if(response.ok) {
@@ -12,7 +15,9 @@ var getYearTopMovies = function(year) {
                     } else {
                         sections = sections.parse.sections
                         var secNum = null
+                        // loop all the sections for that given page
                         for(var i = 0; i < sections.length; i++) {
+                            // check the names of the sections; if they equal any of the ones below, then thats the section number we want
                             if(
                                 sections[i].line === "Highest-grossing films" || 
                                 sections[i].line === "Highest-grossing films (U.S.)" ||
@@ -22,22 +27,29 @@ var getYearTopMovies = function(year) {
                                 break;
                             }
                         }
+                        // now make the same call again except now with the specific section number so we can narrow our search
                         fetch("https://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + year + "_in_film&prop=text&section=" + secNum + "&origin=*&formatversion=2").then(function(response){
                             if(response.ok){
                                 response.json().then(function(data){
+                                    // parse through the html received from the api
                                     data = jQuery.parseHTML(data.parse.text)
+                                    // look for the table
                                     data = $(data[0]).find("table")[0]
                                     data = $(data).children()[1]
                                     data = $(data).children()
+                                    // start an empty array that will hold the movies
                                     var movies = []
                                     for (var i = 1; i < data.length; i++) {
+                                        // loop through every row of the table
                                         var movieHtml = $(data[i]).children()
                                         var movieTitle = $(movieHtml[1]).text().trim()
+                                        // save the movie name and the year of the film
                                         movies.push({
                                             movieTitle: movieTitle,
                                             movieYear: year
                                         })
                                     }
+                                    // now that you have the array of movies, call the omdb api for each movie to receive more details
                                     async function asyncCalltoOMDB() {
                                         var res = await omdbApiCalls(movies)
                                         resolve(res)
@@ -53,8 +65,11 @@ var getYearTopMovies = function(year) {
     })
 }
 
+// this function calls the wiki api and returns an array of movies that were found in the highest grossing films page
 var getDecadeTopMovies = function(decade) {
+    // create new promise that will only resolve when the results are received
     return new Promise(resolve => {
+        // the link we are calling from the api to see what section number we need to look at
         var sectionsUrl = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + decade + "s_in_film&prop=sections&origin=*&formatversion=2"
         fetch(sectionsUrl).then(function(response){
             if(response.ok) {
@@ -67,29 +82,39 @@ var getDecadeTopMovies = function(decade) {
                     } else {
                         sections = sections.parse.sections
                         var secNum = null
+                        // loop through all the available sections of the page
                         for(var i = 0; i < sections.length; i++) {
+                            // now we are only looking for one possible section, if it is there, return the section number
                             if(sections[i].line === "Highest-grossing films"){
                                 secNum = sections[i].index
                                 break;
                             }
                         }
+                        // now that we have the section, repeat the call except now with the section number to narrow the search
                         fetch("https://en.wikipedia.org/w/api.php?action=parse&format=json&page=" + decade + "s_in_film&prop=text&section=" + secNum + "&origin=*&formatversion=2").then(function(response){
                             if(response.ok){
                                 response.json().then(function(data) {
+                                    // parse through the returned html text from the api
                                     data = jQuery.parseHTML(data.parse.text)
+                                    // find the table of all the movies... should be the first or only table of the section
                                     data = $(data[0]).find("table")[0]
                                     data = $(data).children()[1]
                                     data = $(data).children()
+                                    // creat ean empty array that will hold all the movies
                                     var movies = []
                                     for (var i = 1; i < data.length; i++) {
                                         var movieHtml = $(data[i]).children()
+                                        // get the movie title from that row
                                         var movieTitle = $(movieHtml[1]).text().trim()
+                                        // get the movie year from that row
                                         var movieYear = $(movieHtml[movieHtml.length - 2]).text().trim()
+                                        // add it to the array
                                         movies.push({
                                             movieTitle: movieTitle,
                                             movieYear: movieYear
                                         })
                                     }
+                                    // now make an omdb api call for all the movies in the array
                                     async function asyncCalltoOMDB() {
                                         var res = await omdbApiCalls(movies)
                                         resolve(res)
@@ -105,12 +130,16 @@ var getDecadeTopMovies = function(decade) {
     })
 }
 
+// this function is only used for single api calls for individual movies i.e. the movie.html page
 var singleOmdbApiCall = function (title) {
+    // create a new promise to make sure to wait for all the reuslts before returning the value
     return new Promise(resolve => {
         if (typeof title === "string") {
+            // the title variable will either start with i= for imdb number or t= for title string
             fetch("https://www.omdbapi.com/?" + title.trim() + "&apikey=f92c60e5").then(function (response) {
                 if (response.ok) {
                     response.json().then(function (data) {
+                        // return data received from the api
                         resolve(data)
                     })
                 }
@@ -124,32 +153,44 @@ var singleOmdbApiCall = function (title) {
     })
 }
 
+// this function makes omdb api calls several times for the array 
 var omdbApiCalls = function (movies) {
     return new Promise(resolve => {
+        // this map() function will create a pending fetch request for every single element of the array
         var movieFetches = movies.map((movie) => {
+            // this section of code helps handle little bugs we found with the search function of the api
             movie.movieTitle = movie.movieTitle.replace("Star Wars: ", "")
             movie.movieTitle = ((movie.movieTitle === "Seven") ? "Se7en" : movie.movieTitle)
+            // return the fetch for the given movie title and year the film came out
             return fetch("https://www.omdbapi.com/?t=" + movie.movieTitle.split(' ').join('+') + "&y=" + movie.movieYear + "&apikey=f92c60e5")
         })
+        // make a promise for when all the fetches from above are done
         Promise.all(movieFetches).then((movies) => {
+            // now make another map for the .json request
             var moviesArray = movies.map((movie) => {
                 return movie.json()
             })
+            // make yet another promise for when all the .json() requests are done
             Promise.all(moviesArray).then((movies) => {
                 var movieArr = []
+                // loop through every movie
                 movies.forEach(movie => {
                     if(movie.Response === "True") {
+                        // add a moment key value pair that will save the date of the film, so we can sort them
                         movie.releasedDate = moment(movie.Released, "DD MMM YYYY")
                         movieArr.push(movie)
                     }
                 })
+                // sort all the movies from oldest first to newest last (january to decemeber.... 2000 to 2009)
                 sortedMovies = sortByDate(movieArr)
+                // return the objects of all the movies that are now sorted
                 resolve(sortedMovies)
             })
         })        
     })
 }
 
+// This function will sort the movies by release date
 var sortByDate = function(movies) {
     return movies.sort((a, b) => a.releasedDate - b.releasedDate)
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,9 @@ $("#decade-btns").on("click", ".decade", function(){
     // Returns id of decade button pressed
     var decade = $(this).attr("id")
     async function asyncCallforDecades() {
+        // make API calls to return an array of the top movies of the decade
         var results = await getDecadeTopMovies(decade)
+        // call the function that will display the movies in the HTML
         populateMovies(results, true, decade)
     }
     asyncCallforDecades()
@@ -25,7 +27,9 @@ $( "#year-search" ).submit(function( event ) {
     var resultArr = checkIfYear(searchTerm)
     if (resultArr[0]) {
         async function asyncCallforYear() {
+            // make API calls to return an array of the top movies of the year
             var results = await getYearTopMovies(searchTerm)
+            // call the function that will display the movies in the HTML
             populateMovies(results, false, searchTerm)    
         }
         asyncCallforYear()
@@ -43,14 +47,17 @@ $("#movies-display").on("click", ".exit-btn", function(){
 
 // Populating list of movies onto the html
 var populateMovies = function(movies, decade, year) {
+    // get the movie display section by id
     var moviesDisplay = $("#movies-display")
     moviesDisplay.removeClass("d-none")
+    // empty all children
     moviesDisplay.empty()
     if(decade) {
         var title = "Most Popular Movies from the " + year + "'s"
     } else {
         var title = "Most Popular Movies from " + year
     }
+    // Put the title of the section
     moviesDisplay.append(
         $("<div>").addClass("display-header").html(
             `<h2>` + title + `</h2>
@@ -59,27 +66,37 @@ var populateMovies = function(movies, decade, year) {
             </span>`
         )
     )
+    // this variable will hold the current month and year of where we are in the array i.e. july 2005
     var currentMonth = ""
+    // loop through all movies in the array
     movies.forEach(movie => {
+        // get the release date of the movie
         var movieMonth = movie.releasedDate.format("MMMM, YYYY")
+        // see if the release date is different than the current month above
         if (currentMonth != movieMonth) {
             currentMonth = movieMonth
+            // if it is different, create a new header for the new month i.e. august 2005
             moviesDisplay.append(
                 $("<div>").addClass("display-date").html(
                     `<h4>` + currentMonth + `</h4>`
                 )
             )
         }
+        // creat a movie div that will hold all the contents and information for that paticular movie
+        // include the movie poster... if there isnt a poster url available, do the default TM logo
         var movieContainer = $("<div>").addClass("movie-list-item").attr("id", movie.imdbID).html(
             `<img src="` + ((movie.Poster ==="N/A") ? "./assets/images/default.png" : movie.Poster) + `" alt="` + movie.Title + ` Movie Poster" class="list-movie-poster">`)
+        // create a div that will hold the individual movie information
         var movieInformation = $("<div>").addClass("movie-list-item-info")
         movieInformation.append($("<h3>").text(movie.Title))
+        // if there is a genre display it etc. etc. etc.
         if(movie.Genre != "N/A") {
             movieInformation.append($("<p>").text(movie.Genre))
         }
         if(movie.Rated != "N/A") {
             movieInformation.append($("<p>").text(movie.Rated))
         }
+        // if there are ratings, loop through them and only display imdb and rotten tomatoes
         if(movie.Ratings.length >=0) {
             movie.Ratings.forEach(rating => {
                 if (rating.Source === "Internet Movie Database") {
@@ -99,6 +116,7 @@ var populateMovies = function(movies, decade, year) {
                 }
             })
         }
+        // display the right arrow icon to indicate a button
         var iconSpan = $("<span>").addClass("material-icons").text("arrow_forward_ios")
         movieContainer.append(movieInformation, iconSpan)
         moviesDisplay.append(movieContainer)
@@ -128,14 +146,17 @@ var checkIfYear = function(year) {
 
 // Search function for movie.html (single movie search feature)
 var populateMovieInfo = function(movieInfo) {
+    // get the release date so we can reformat how the date is displayed
     var releasedDate = moment(movieInfo.Released, "DD MMM YYYY")
+    // diplay the movie poster, if there isnt a url available, display the default TM logo
     $("#movie-poster").attr("src", ((movieInfo.Poster != "N/A") ? movieInfo.Poster : "./assets/images/default.png" ))
+    // the rest is similar to above populateMovies() function
     $("#movie-title").text(movieInfo.Title)
     $("#movie-genre").text(((movieInfo.Genre != "N/A") ? movieInfo.Genre : ""))
     $("#movie-rated").text(((movieInfo.Rated != "N/A") ? movieInfo.Rated : ""))
     $("#movie-release").text(releasedDate.format("MMMM DD, YYYY"))
     $("#movie-time").text(((movieInfo.Runtime != "N/A") ? ("Length: " + movieInfo.Runtime) : ""))
-
+    // same as above for looping through and only grabbing IMDB and rotten tom rating
     if(movieInfo.Ratings.length >= 0) {
         movieInfo.Ratings.forEach(rating => {
             if (rating.Source === "Internet Movie Database") {
@@ -155,13 +176,11 @@ var populateMovieInfo = function(movieInfo) {
             }
         })
     }
-    
-
+    // get the rest of the movie information
     $("#movie-director").text(((movieInfo.Director != "N/A") ? movieInfo.Director : ""))
     $("#movie-actors").text(((movieInfo.Actors != "N/A") ? movieInfo.Actors : ""))
     $("#movie-plot").text(((movieInfo.Plot != "N/A") ? movieInfo.Plot : ""))
     $("#movie-awards").text(((movieInfo.Awards != "N/A") ? movieInfo.Awards : ""))
-    
     
     $("#default-img").addClass("d-none")
     $("#error-img").addClass("d-none")
@@ -188,18 +207,23 @@ $( "#movie-search" ).submit(function( event ) {
 });
 
 // This function uses url query params to figure out what movie to search for
-if(document.URL.indexOf("movie.html") >= 0){ 
+if(document.URL.indexOf("movie.html") >= 0){
+    // when the page loads, if it is the movie.html page, do everything in this function
     $("#movie-display").addClass("d-none")
     $("#error-img").addClass("d-none")
     $("#default-img").removeClass("d-none")
-    
+
+    // get the search string i.e. t=shrek or i=tt01234
     var searchTerm = window.location.search.substring(1)
     if(searchTerm) {
+        // call the api for the individual movie
         async function asyncCallforMovie() {
             var results = await singleOmdbApiCall(searchTerm)
             if (results.Response === "True") {
+                // if a result was found populate the movie info
                 populateMovieInfo(results) 
             } else {
+                // if a result was not found, display the error img
                 populateError(results)
             }
         }
